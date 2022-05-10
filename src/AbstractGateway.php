5540fccce7111248499d945911b7aef3b58ada1d
Max <max@Maxs-MacBook-Pro.local>
2022-05-10 11:31:47 +0100

    removed unwanted parameters

--- src/AbstractGateway.php
+++ src/AbstractGateway.php
@@ -13,15 +13,6 @@ abstract class AbstractGateway extends OmnipayAbstractGateway implements Constan
     public function getDefaultParameters()
     {
         return [
-            'vendor' => null,
-            'testMode' => false,
-            'referrerId' => null,
-            'language' => null,
-            'useOldBasketFormat' => false,
-            'exitOnResponse' => false,
-            'apply3DSecure' => null,
-            'useAuthenticate' => null,
-            'accountType' => null,
             'application_id' => null,
             'access_token' => null,
             'location_id' => null
